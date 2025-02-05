e2748bdda6454e32bd2cc5cd9346c6b3400172de
chenlx <chenlx@broada.com>
2025-02-05 22:13:58 +0800

    feat: "项目初始化 - 前端"

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const msg = "欢迎来到图管家协助平台，你将从这里开始您的创作之旅~";
+</script>
+<style scoped>
+#homePage {
+}
+</style>
