ebaa49b20a6c8de64d991a971feae769f4eeeeda
chenlx <chenlx@broada.com>
2025-02-06 10:45:02 +0800

    feat: "用户模块 - 功能开发"

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -8,7 +8,7 @@
         <router-view />
       </a-layout-content>
       <a-layout-footer class="footer">
-        <a href="https://www.codefather.cn" target="_blank"> 编程导航 by 程序员鱼皮 </a>
+        <a href="https://www.baidu.com" target="_blank"> 百度一下，你就知道~ </a>
       </a-layout-footer>
     </a-layout>
   </div>
